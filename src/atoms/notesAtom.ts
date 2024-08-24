3a2edb43d72ff002ac1107287b314ef1199d8c50
rioto.nagano <kingdom950927@gmail.com>
2024-08-24 09:58:42 +0900

    feat:selectorを作成

--- src/atoms/notesAtom.ts
+++ src/atoms/notesAtom.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const textState = atom({
     key: 'textState',
@@ -8,4 +8,14 @@ export const textState = atom({
 export const fontSizeState = atom({
   key: 'fontSizeState',
   default: 14,
+});
+
+export const fontSizeLabelState = selector({
+  key: 'fontSizeLabelState',
+  get: ({get}) => {
+    const fontSize = get(fontSizeState);
+    const unit = 'px';
+
+    return `${fontSize}${unit}`;
+  },
 });
--- src/components/FontButton.tsx
+++ src/components/FontButton.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { fontSizeState } from '../atoms/notesAtom.ts';
+import { fontSizeState, fontSizeLabelState } from '../atoms/notesAtom.ts';
 
 const FontButton = () => {
 
   const [fontSize, setFontSize] = useRecoilState(fontSizeState);
+  const fontSizeLabel = useRecoilValue(fontSizeLabelState);
 
   console.log(fontSize);
 
   return (
     <ButtonWrapper>
+      <div>Current font size: {fontSizeLabel}</div>
+
       <button onClick={() => setFontSize((size: number) => size + 1)} style={{ fontSize: `${fontSize}px` }}>
         Click to Enlarge
       </button>
